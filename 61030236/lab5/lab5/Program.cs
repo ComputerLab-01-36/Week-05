13a63e4e01215fc49491bbd087060b7ee5d2afe6
Toeyty <61030236@kmitl.ac.th>
2019-10-05 12:20:58 +0700

    1.1.3. คำสั่ง throw

--- 61030236/lab5/lab5/Program.cs
+++ 61030236/lab5/lab5/Program.cs
@@ -10,6 +10,13 @@ namespace lab5
     {
         static void Main(string[] args)
         {
+            int a = 10;
+            int b = 20;
+            int c = add(a, b);
+        }
+        private static int add(int a, int b)
+        {
+            throw new NotImplementedException();
         }
     }
 }
